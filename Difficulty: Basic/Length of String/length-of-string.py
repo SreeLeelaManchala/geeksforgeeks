ace68623d90cda966db03a8133e0ee0af8e17901
SreeLeelaManchala <156211619+SreeLeelaManchala@users.noreply.github.com>
2025-03-08 14:01:31 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Length of String/length-of-string.py
@@ -0,0 +1,22 @@
+#User function Template for python3
+
+class Solution:
+    def lengthString(self, str):
+        return len(str)
+    
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+#contributed by RavinderSinghPB
+if __name__ =='__main__':
+    tcs = int(input())
+    
+    for _ in range(tcs):
+        s=input()
+        
+        print(Solution().lengthString(s))
+        print("~")
+# } Driver Code Ends
